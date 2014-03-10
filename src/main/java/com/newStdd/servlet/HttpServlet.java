4ceabe40289abdfd8b7fbb728d7f8843fd355749
doul <doulcn@163.com>
2014-03-10 14:49:45 +0800

    test servlet

---
+++ src/main/java/com/newStdd/servlet/HttpServlet.java
@@ -0,0 +1,37 @@
+package com.newStdd.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HttpServlet
+ */
+@WebServlet("/HttpServlet")
+public class HttpServlet extends javax.servlet.http.HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see javax.servlet.http.HttpServlet#javax.servlet.http.HttpServlet()
+     */
+    public HttpServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().println("get");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().println("post");
+	}
+
+}
